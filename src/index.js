e8ce9fd8e86853924eed77f24bfff02ce8d08f5f
Nariman <mirmanov1@mail.ru>
2023-09-02 15:38:21 +0600

    add template

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import App from "./components/app/app";
+
 import "./index.css";
-import App from "./App";
 
-const elem = <h2>hello world!</h2>;
-
-// const elem = React.createElement(
-//   "h2",
-//   { className: "greeting" },
-//   "hello from React"
-// );
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elem);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
